292fd78ad3ec676fef9cdeda63d2ba81f919aa66
Caue Paiva <128509727+caue-paiva@users.noreply.github.com>
2024-02-12 12:17:45 -0300

    Update vm_setup.bash

--- vm_setup/vm_setup.bash
+++ vm_setup/vm_setup.bash
@@ -2,8 +2,8 @@
 PYTHON_VERSION= "3.9.16"
 AIRFLOW_VERSION= "2.8.1"
 PROJECT_NAME="crypto_data"
-#SCRIPT_DIR="$(dirname "$(realpath "$0")")"
-#PARENT_DIR="$(dirname "$script_dir")"
+#SCRIPT_DIR=
+#PARENT_DIR="${dirname "$script_dir"}"
 
 yes | sudo dnf update #update the system
 yes | sudo dnf install python3-pip #install python and git, [ yes | ] answers any prompt to the command with yes 
@@ -12,23 +12,27 @@ yes | sudo dnf install git
 git clone https://github.com/caue-paiva/airflow_project -b vm_branch
 cd airflow_project/
 cd vm_setup/
+MAIN_WORK_DIR="${pwd}" #main dir we will use for the project, named vm_setup
 
 python3 -m venv airflow_env
-source airflow_env/bin/activate
+source /airflow_env/bin/activate
 pip install airflowctl
 
-airflowctl init $(PROJECT_NAME) --airflow-version $(AIRFLOW_VERSION) --python-version $(PYTHON_VERSION) #inits the airflowctl project 
-cd $(PROJECT_NAME)
-$(PROJECT_NAME) build #builds project 
+airflowctl init ${PROJECT_NAME} --airflow-version ${AIRFLOW_VERSION} --python-version ${PYTHON_VERSION} #inits the airflowctl project 
+cd ${PROJECT_NAME}
+${PROJECT_NAME} build #builds project 
 cd ..
-airflowctl start $(PROJECT_NAME) --background 
+airflowctl start ${PROJECT_NAME} --background 
 
-cd $(PROJECT_NAME)/dags/ 
+cd ${PROJECT_NAME}/dags/ 
 mkdir include #creating folder for python code not used by the dags
 cd ..
 cd ..
 
 mv binance_api.py crypto_data_etl.py my_airflow_project/dags/include/  #move auxiliary python files to include folder
+
+deactivate #deactivates current env only used to isolated airflowctl installation
+source ${MAIN_WORK_DIR}/${PROJECT_NAME}/.venv/bin/pip install -r ${MAIN_WORK_DIR}/_requirements.txt
 
 #------ setting up airflow variables and connections, needs to be done in a airflowctl project dir
 
@@ -41,8 +45,8 @@ airflowctl airflow variables set --description "ow many minutes of data does eac
 
 #--connections
 
-# 1) export all connections to a file using airflowctl airflow connections export $(file)
+# 1} export all connections to a file using airflowctl airflow connections export ${file}
 
-# 2) Get the specific AWS connection (DO NOT SHARE IT ON GITHUB) and make a json file only with it (or keep the others ununsed connections)
+# 2} Get the specific AWS connection {DO NOT SHARE IT ON GITHUB} and make a json file only with it {or keep the others ununsed connections}
 
-# 3) Import the connections file using airflowctl airflow connections import $(file)
+# 3} Import the connections file using airflowctl airflow connections import ${file}
